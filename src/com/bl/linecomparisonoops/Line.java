064a7306cea635f914d03329702019f3e4bbdd72
akshaydig <you@example.com>
2021-12-13 16:39:18 +0530

    Added: functionality to find two lines are equal or not

--- src/com/bl/linecomparisonoops/Line.java
+++ src/com/bl/linecomparisonoops/Line.java
@@ -1,9 +1,16 @@
 package com.bl.linecomparisonoops;
 
 public class Line {
-	Point p1, p2;
+	Point p1, p2, p3, p4;
 
-	public double getLength() {
-		return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+	public double getLength(Point r, Point s) {
+		return Math.sqrt((r.x - s.x) * (r.x - s.x) + (r.y - s.y) * (r.y - s.y));
+	}
+
+	public boolean equals(Double x, Double y) {
+		if (x.equals(y))
+			return true;
+		else
+			return false;
 	}
 }
--- src/com/bl/linecomparisonoops/Main.java
+++ src/com/bl/linecomparisonoops/Main.java
@@ -3,17 +3,32 @@ package com.bl.linecomparisonoops;
 public class Main {
 	public static void main(String[] args) {
 		Point p1 = new Point();
-		p1.x = 4;
+		p1.x = 5;
 		p1.y = 3;
 		Point p2 = new Point();
 		p2.x = 3;
 		p2.y = 2;
+		Point p3 = new Point();
+		p3.x = 5;
+		p3.y = 3;
+		Point p4 = new Point();
+		p4.x = 3;
+		p4.y = 2;
+
+		Line lineOne = new Line();
+		lineOne.p1 = p1;
+		lineOne.p2 = p2;
+		Line lineTwo = new Line();
+		lineTwo.p3 = p3;
+		lineTwo.p4 = p4;
+
+		Double lengthOne = lineOne.getLength(p1, p2);
+		Double lengthTwo = lineTwo.getLength(p3, p4);
 
 		Line line = new Line();
-		line.p1 = p1;
-		line.p2 = p2;
-
-		double length = line.getLength();
-		System.out.println("Length of Line is :" + length);
+		if (line.equals(lengthOne, lengthTwo) == true)
+			System.out.println("Two lines are Equal");
+		else
+			System.out.println("Two lengths are not equal");
 	}
 }
